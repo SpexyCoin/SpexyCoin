5c4da847fe1b9a7cf05d97174494db1600b71d3f
SpookyPool <vbemailhacker@gmail.com>
2020-02-26 17:42:51 +0100

    TX

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -48,7 +48,7 @@ namespace CryptoNote
         static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
         const uint64_t GENESIS_BLOCK_REWARD                     =             100'000'000'00000000;
-        const char     GENESIS_COINBASE_TX_HEX[] = "012801ff0001808084fea6dee11102d936b6bd850387a5999bcb5f8171f6957130117f243891abaabdbc3f7fb04c482101c056455ad0e9b13346a1cc006499c1ab945eb64fdd5da8cb6d5022f36af9491f";
+        const char     GENESIS_COINBASE_TX_HEX[] = "012801ff0001808084fea6dee11102aee369549971213255c9aed76e2f3edb3914bfb54d2108044a9f9b9e275a5376210136b94f477c429b04ce61b585ba04afa879bbc23911b76018c2215edd33de5665";
 
         static_assert(sizeof(GENESIS_COINBASE_TX_HEX) / sizeof(*GENESIS_COINBASE_TX_HEX) != 1, "GENESIS_COINBASE_TX_HEX must not be empty.");
 
