bdf043596c13ec059e45322e192bf535260ca8d0
SpookyPool <vbemailhacker@gmail.com>
2020-02-26 17:41:12 +0100

    Made a mistake?

--- src/cryptonotecore/Currency.cpp
+++ src/cryptonotecore/Currency.cpp
@@ -189,10 +189,7 @@ namespace CryptoNote
         uint64_t &reward,
         int64_t &emissionChange) const
     {
-        assert(alreadyGeneratedCoins <= m_moneySupply);
-        assert(m_emissionSpeedFactor > 0 && m_emissionSpeedFactor <= 8 * sizeof(uint64_t));
-
-        uint64_t baseReward = ((m_moneySupply - alreadyGeneratedCoins) >> m_emissionSpeedFactor);
+        uint64_t baseReward = 0'00100000;
 
         logger(TRACE) << "Base reward " << baseReward;
 
